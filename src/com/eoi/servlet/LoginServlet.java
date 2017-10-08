dddb8a7e4d498ce8847737b4cc40cdc6a5330bb2
whu-lyd <3332639713@qq.com>
2017-10-08 14:36:27 +0800

    change log

---
+++ src/com/eoi/servlet/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.eoi.servlet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.eoi.biz.LoginBiz;
+
+/**
+ * 管理员登录
+ * @author xjf
+ *
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String[]> map = request.getParameterMap();
+
+        HashMap<String, String> result = new LoginBiz().checklogin(map);
+
+        if(result != null){
+            HttpSession session = request.getSession();
+            session.setAttribute("login_info", result);
+            response.getWriter().append("s");
+        }else {
+            response.getWriter().append("f");
+        }
+    }
+
+}
